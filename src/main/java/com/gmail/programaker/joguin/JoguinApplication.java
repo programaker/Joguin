6117494db5e8556fb53a6981a84cbfb79847fa75
programaker <programaker@gmail.com>
2018-08-19 13:52:50 -0300

    Joguin is born

---
+++ src/main/java/com/gmail/programaker/joguin/JoguinApplication.java
@@ -0,0 +1,12 @@
+package com.gmail.programaker.joguin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JoguinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JoguinApplication.class, args);
+	}
+}
