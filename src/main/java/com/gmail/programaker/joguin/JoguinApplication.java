4dc82571e6d07b11264ad5358c564276ef839cc7
programaker <programaker@gmail.com>
2018-08-19 21:31:51 -0300

    Implementing CommandLineRunner

--- src/main/java/com/gmail/programaker/joguin/JoguinApplication.java
+++ src/main/java/com/gmail/programaker/joguin/JoguinApplication.java
@@ -1,10 +1,15 @@
 package com.gmail.programaker.joguin;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class JoguinApplication {
+public class JoguinApplication implements CommandLineRunner {
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println(">>> YAY!");
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(JoguinApplication.class, args);
