ee4be16cb42eadb7b726f74991700cb0a9335347
Marcelo Gomes <programaker@gmail.com>
2018-08-21 15:59:35 -0300

    Reusing blackHoleConsole

--- src/test/java/com/gmail/programaker/joguin/TestConfig.java
+++ src/test/java/com/gmail/programaker/joguin/TestConfig.java
@@ -3,7 +3,10 @@ package com.gmail.programaker.joguin;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.function.Consumer;
+
 @Configuration
 @ComponentScan(basePackages = "com.gmail.programaker.joguin")
 public class TestConfig {
+    public static final Consumer<String> blackHoleConsole = message -> {};
 }
