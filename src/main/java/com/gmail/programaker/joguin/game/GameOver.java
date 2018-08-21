c442c8d1a8d2d43312432088c3fde12c1fe22a04
Marcelo Gomes <programaker@gmail.com>
2018-08-21 15:58:04 -0300

    GameOver step

---
+++ src/main/java/com/gmail/programaker/joguin/game/GameOver.java
@@ -0,0 +1,16 @@
+package com.gmail.programaker.joguin.game;
+
+import java.util.Iterator;
+import java.util.function.Consumer;
+
+public final class GameOver implements GameStep {
+    @Override
+    public GameStep interactWithPlayer(Consumer<String> println, Iterator<String> playerAnswers) {
+        return this;
+    }
+
+    @Override
+    public boolean gameOver() {
+        return true;
+    }
+}
