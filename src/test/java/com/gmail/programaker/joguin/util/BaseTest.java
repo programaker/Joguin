27207f926c5cd31f603161e4a993e59bc93eba5d
Marcelo Gomes <programaker@gmail.com>
2018-08-23 14:51:40 -0300

    Default locale setup for tests

--- src/test/java/com/gmail/programaker/joguin/util/BaseTest.java
+++ src/test/java/com/gmail/programaker/joguin/util/BaseTest.java
@@ -2,11 +2,18 @@ package com.gmail.programaker.joguin.util;
 
 import com.gmail.programaker.joguin.config.MessageConfig;
 import com.gmail.programaker.joguin.config.TestConfig;
+import org.junit.BeforeClass;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import java.util.Locale;
+
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = {TestConfig.class, MessageConfig.class})
 public abstract class BaseTest {
+    @BeforeClass
+    public static void setLocale() {
+        Locale.setDefault(Locale.US);
+    }
 }
