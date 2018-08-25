bde4eb3cd3e83db2c093fe9922c3d442a0392c13
programaker <programaker@gmail.com>
2018-08-24 22:41:18 -0300

    Testing invalid inputs

--- src/test/java/com/gmail/programaker/joguin/game/ExploreTest.java
+++ src/test/java/com/gmail/programaker/joguin/game/ExploreTest.java
@@ -18,10 +18,11 @@ import static org.junit.Assert.*;
 
 public class ExploreTest extends BaseTest {
     private final String london = "1. \uD83D\uDC7D London - UK";
-    private final String tokio = "2. \uD83D\uDC7D Tokyo - Japan";
+    private final String tokyo = "2. \uD83D\uDC7D Tokyo - Japan";
     private final String saoPaulo = "3. \uD83D\uDC7D São Paulo - Brazil";
     private final String savedSaoPaulo = "3. \uD83C\uDF0D São Paulo - Brazil";
     private final String whereToGo = "\nWhere do you want to go? - from 1 to 3 or (Q)uit";
+    private final String invalidOption = "Invalid option";
 
     @Autowired
     private Explore explore;
@@ -38,7 +39,7 @@ public class ExploreTest extends BaseTest {
         int i = 0;
         assertEquals("", fakeConsole.get(i++));
         assertEquals(london, fakeConsole.get(i++));
-        assertEquals(tokio, fakeConsole.get(i++));
+        assertEquals(tokyo, fakeConsole.get(i++));
         assertEquals(saoPaulo, fakeConsole.get(i++));
         assertEquals(whereToGo, fakeConsole.get(i++));
     }
@@ -55,7 +56,7 @@ public class ExploreTest extends BaseTest {
         int i = 0;
         assertEquals("", fakeConsole.get(i++));
         assertEquals(london, fakeConsole.get(i++));
-        assertEquals(tokio, fakeConsole.get(i++));
+        assertEquals(tokyo, fakeConsole.get(i++));
         assertEquals(savedSaoPaulo, fakeConsole.get(i++));
         assertEquals(whereToGo, fakeConsole.get(i++));
     }
@@ -68,6 +69,36 @@ public class ExploreTest extends BaseTest {
         );
 
         assertEquals("Should have gone to Quit step", "Quit", nextStep.name());
+    }
+
+    @Test
+    public void givenWrongOption() {
+        List<String> fakeConsole = new ArrayList<>();
+
+        explore.start(beginProgress()).interactWithPlayer(
+            fakeConsole::add,
+            Arrays.asList("0", "a", " ", "9", "1").iterator()
+        );
+
+        int i = 0;
+
+        assertEquals("", fakeConsole.get(i++));
+        assertEquals(london, fakeConsole.get(i++));
+        assertEquals(tokyo, fakeConsole.get(i++));
+        assertEquals(saoPaulo, fakeConsole.get(i++));
+        assertEquals(whereToGo, fakeConsole.get(i++));
+
+        assertEquals(invalidOption, fakeConsole.get(i++));
+        assertEquals(whereToGo, fakeConsole.get(i++));
+
+        assertEquals(invalidOption, fakeConsole.get(i++));
+        assertEquals(whereToGo, fakeConsole.get(i++));
+
+        assertEquals(invalidOption, fakeConsole.get(i++));
+        assertEquals(whereToGo, fakeConsole.get(i++));
+
+        assertEquals(invalidOption, fakeConsole.get(i++));
+        assertEquals(whereToGo, fakeConsole.get(i++));
     }
 
     private GameProgress beginProgress() {
@@ -83,7 +114,7 @@ public class ExploreTest extends BaseTest {
     }
 
     private GameProgress saveSaoPaulo(GameProgress gameProgress) {
-        gameProgress.getInvasions().get(2).setAliensDefeated();
+        gameProgress.getInvasions().get(2).setAliensDefeated(true);
         return gameProgress;
     }
 }
